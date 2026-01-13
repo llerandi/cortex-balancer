d471fa0d2c6de1c48da06af03b50739802ffa8f0
Edward <llerandi@users.noreply.github.com>
2026-01-13 20:11:02 +0100

    feat: worker "work" controller with the imports

---
+++ services/worker-service/src/main/java/com/example/cortexbalancer/worker_service/WorkController.java
@@ -0,0 +1,8 @@
+package com.example.cortexbalancer.worker_service;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ThreadLocalRandom;
