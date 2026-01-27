f38052000b05bbfc07e2d3c9ce4670fa1ec425c7
Edward <llerandi@users.noreply.github.com>
2026-01-27 19:03:58 +0100

    feat: Spring Initializr quickstart generated project
    Dependencies: Spring Web + Spring Reactive Web

---
+++ services/environment-api/src/test/java/com/example/cortexbalancer/environment_api/EnvironmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cortexbalancer.environment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
